a44ea593d6872640c3bc1c09a8207f542959b114
Alex <alexis.rj2110@gmail.com>
2025-08-01 18:16:06 -0500

    Igualacin 2

--- ejemplo5/proyectoUno/administrativo/forms.py
+++ ejemplo5/proyectoUno/administrativo/forms.py
@@ -3,19 +3,20 @@ from django.utils.translation import gettext_lazy as _
 from django import forms
 
 from administrativo.models import Estudiante, \
-        NumeroTelefonico
+    NumeroTelefonico
+
 
 class EstudianteForm(ModelForm):
     class Meta:
         model = Estudiante
         fields = ['nombre', 'apellido', 'cedula', 'correo']
+
         labels = {
             'nombre': _('Ingrese nombre por favor'),
             'apellido': _('Ingrese apellido por favor'),
             'cedula': _('Ingrese cédula por favor'),
             'correo': _('Ingrese correo por favor'),
         }
-
 
     def clean_nombre(self):
         valor = self.cleaned_data['nombre']
@@ -52,6 +53,31 @@ class NumeroTelefonicoForm(ModelForm):
     class Meta:
         model = NumeroTelefonico
         fields = ['telefono', 'tipo', 'estudiante']
+
+        labels = {
+            'telefono': _('Ingrese número de teléfono'),
+            'tipo': _('Seleccione tipo de teléfono'),
+            'estudiante': _('Seleccione estudiante'),
+        }
+
+        def clean_telefono(self):
+            valor = self.cleaned_data['telefono']
+            if len(valor) != 10:
+                raise forms.ValidationError("Ingrese número de teléfono con 10 dígitos")
+            return valor
+
+        def clean_tipo(self):
+            valor = self.cleaned_data['tipo']
+            consonantes = "bcdfghjklmnpqrstvwxyz"
+            if valor[0].lower() not in consonantes:
+                raise forms.ValidationError("El tipo de teléfono debe empezar con una consonante")
+            return valor
+
+        def clean_estudiante(self):
+            valor = self.cleaned_data['estudiante']
+            if not valor:
+                raise forms.ValidationError("Debe seleccionar un estudiante")
+            return valor
 
 
 class NumeroTelefonicoEstudianteForm(ModelForm):
